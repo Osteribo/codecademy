1c692b3ff14b9a5058844c902f2dbd7447d01f0d
Aaron <osteribo@outlook.com>
2019-07-09 16:23:08 -0700

    This is the completed hash map project from codecademy

---
+++ computer_science_projects/Blossom_Solver/Readme.txt
@@ -0,0 +1,5 @@
+Blossom is a codecademy Hash Map project under the section of complex data structures
+
+this python project combines Hash Maps and linked lists to relate the names of flowers with their meanings.
+
+collisions are avided using separate chaining
---
+++ computer_science_projects/Blossom_Solver/blossom_lib.py
@@ -0,0 +1,3 @@
+#This the list of flowers and their meanings
+
+flower_definitions = [['begonia', 'cautiousness'], ['chrysanthemum', 'cheerfulness'], ['carnation', 'memories'], ['daisy', 'innocence'], ['hyacinth', 'playfulness'], ['lavender', 'devotion'], ['magnolia', 'dignity'], ['morning glory', 'unrequited love'], ['periwinkle', 'new friendship'], ['poppy', 'rest'], ['rose', 'love'], ['snapdragon', 'grace'], ['sunflower', 'longevity'], ['wisteria', 'good luck']]
