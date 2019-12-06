d133ea981515cd2f9695699f1b2f300616260410
Aaron Oster <alo@Aarons-MacBook-Pro.local>
2019-12-06 07:01:00 -0800

    started minesweeper project folder structure and added minesweeper.js file with some hardcoded logic

---
+++ front_end_intens/minesweeper/src/minesweeper.js
@@ -0,0 +1,7 @@
+// this file holds the js logic for the minesweeper game
+
+const blankLine = ' | | ';
+const guessLine = '1| | '
+
+console.log ('this is what the gameboard will look like');
+console.log(blankLine);
